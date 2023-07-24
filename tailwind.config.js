e086c257b1d8720d6a068a9495899420bdf10b2d
nobata <ahirururu122@gmail.com>
2023-07-25 00:13:05 +0900

    first

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "san-serif"],
+      },
+      colors: {
+        body: "#050505",
+        "selected-text": "#A3A3FF",
+        theme: "#5c318c",
+        secondary: "#9191A4",
+        badge: "#3F3F51",
+      },
+    },
+  },
+  plugins: [],
+};
